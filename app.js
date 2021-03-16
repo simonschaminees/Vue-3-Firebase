eb2facf9f487d37d32354da2eb9e15c2824b47e0
simonschaminees <simon.schaminee@hotmail.com>
2021-03-16 19:39:12 +0100

    hide and show books

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const app = Vue.createApp({
   data() {
     return {
+      showBooks: true,
       title: 'The Way of Kings',
       author: 'Brandon Sanderson',
       age: 45
     }
   },
   methods: {
-    changeTitle(title) {
-      // this.title = 'Words of Radiance'
-      this.title = title
-    }
+    toggleShowBooks(){
+      this.showBooks = !this.showBooks
+    }    
   }
 })
 
